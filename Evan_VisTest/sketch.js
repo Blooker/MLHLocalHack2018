455ba816c92dad391c9ef32aad4209def03bfe7b
Evan Griffiths <egrif001@gold.ac.uk>
2018-12-01 13:59:54 +0000

    Evan_VisTest: Play button now works on page

--- Evan_VisTest/sketch.js
+++ Evan_VisTest/sketch.js
@@ -1,39 +1,53 @@
 var song;
 
+var preloadDone = false;
+
 var currentScale;
 
 var vol;
 
+var controls;
+var playButton;
+
 function preload() {
     song = loadSound("audio/audioTest.mp3");
+    controls = select("#controls");
+    playButton = select("#playButton");
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    preloadDone = true;
+
+    createCanvas(windowWidth, windowHeight, WEBGL);
     frameRate(60);
-    
+
     song.play();
+    toggleAudio();
+
     amp = new p5.Amplitude();
-    
+
+    ambientLight(50);
+
     updateVisSize();
 }
 
-function draw() {
-    translate(windowWidth/2, windowHeight/2);
-    
-    background(0);
-    
-    scale(currentScale);
-    
-    vol = amp.getLevel();
-    
-    ellipse(0, 0, 800, vol*1600);
-}
+function draw() {    
+    background(0); 
+    var dirY = (mouseY / height - 0.5) *2;
+    var dirX = (mouseX / width - 0.5) *2;
+    directionalLight(250, 250, 250, -dirX, -dirY, -0.25);
 
-function keyPressed() {
-    if (keyCode === 32) {
-        toggleAudio();
-    }
+    vol = amp.getLevel();
+
+    noStroke();
+
+    scale(currentScale);
+    ambientMaterial(250);
+    sphere(vol*800);
+    scale(1/currentScale);
+
+    controls.position((windowWidth/2)-25, windowHeight-75);
+    playButton.mouseClicked(toggleAudio);
 }
 
 function windowResized() {
@@ -46,9 +60,16 @@ function updateVisSize () {
 }
 
 function toggleAudio () {
+    if (!preloadDone)
+        return;
+
     if (song.isPlaying()) {
         song.pause();
+        select('.fa-pause').hide();
+        select('.fa-play').show();
     } else {
         song.play();
+        select('.fa-play').hide();
+        select('.fa-pause').show();
     }
 }
