b009d89f78f2392b5fc7523c381d76b98153ac23
TristanThomson <tthom001@gold.ac.uk>
2018-12-01 18:46:23 +0000

    added square logos

---
+++ front_end/script.js
@@ -0,0 +1,9 @@
+function setup () {
+    console.log("HEY");
+    var logo = document.getElementsByClassName("logo");
+    
+    for(var i=0;i<logo.length;i++){
+        console.log(logo[i]);
+    }
+}
+
