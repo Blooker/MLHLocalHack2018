abd23b9f80395b86f76dfbc6d834f142f25e8df8
Evan Griffiths <egrif001@gold.ac.uk>
2018-12-01 11:45:33 +0000

    Evan_VisTest: ellipse grows when mic is talked into (sticks to center of page and resizes based on window width)

--- Evan_VisTest/sketch.js
+++ Evan_VisTest/sketch.js
@@ -1,7 +1,32 @@
+var mic;
+var song;
+
+var currentScale;
+
 function setup() {
-  // put setup code here
+    createCanvas(windowWidth, windowHeight);
+    mic = new p5.AudioIn();
+    mic.start();
+    
+    updateVisSize();
 }
 
 function draw() {
-  // put drawing code here
+    translate(windowWidth/2, windowHeight/2);
+    
+    background(0);
+    
+    scale(currentScale);
+    
+    var vol = mic.getLevel();
+    ellipse(0, 0, 800, vol*1600);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    updateVisSize();
+}
+
+function updateVisSize () {
+    currentScale = (windowWidth/1920);
 }
